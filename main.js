64bf4212e30432ec648979604c2944a73abc9e0e
timo9939 <timothylau20199@gmail.com>
2023-12-28 00:28:55 +0000

    created a function to calculat the exchange rate

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ let base_currency ="GBP"
 let target_currency="EUR"
 let rate=$("#rate")
 let result=$("#target_amount")
+let inputAmount=$("#amount")
 
   // let GBPinput=prompt("Enter GBP amount")
   // console.log("GBP amount is", GBPinput)
@@ -11,7 +12,15 @@ let result=$("#target_amount")
 fetch('https://v6.exchangerate-api.com/v6/'+api+'/pair/'+base_currency+'/'+target_currency)
 .then(res => res.json())
 .then(data => {console.log(data);
-rate.text(data.conversion_rate);}
+rate.text(data.conversion_rate);
+}
+
 )
-.then()
 .catch(err=>err())
+
+function convert(){
+    console.log("inputAmount is", inputAmount.val());
+    let target_result = inputAmount.val() * parseFloat(rate.text());
+    console.log("target result is", target_result);
+    result.text(target_result)
+}
