ed25460fb86a002adc1bcf50ecd558ed629bdb57
timo9939 <timothylau20199@gmail.com>
2024-01-08 22:46:48 +0000

    caculation of rate*inputAmount does not work

--- main.js
+++ main.js
@@ -1,29 +1,33 @@
 let currency_base="GBP"
 let api="aebc0b80457164b375c7f496"
-let base_currency ="GBP"
-let target_currency="EUR"
+let base_currency =""
+let target_currency=""
 let rate=$("#rate")
 let result=$("#target_amount")
 let inputAmount=$("#amount")
 const fromLabel= $("#base_currency_container")
 const toLabel=$('#target_currency_container')
+let convert_Btn=$('#convertBtn')
+
+
+
 
   // let GBPinput=prompt("Enter GBP amount")
   // console.log("GBP amount is", GBPinput)
  
 import {countryList} from "./countryCode.js"
 $.each(countryList, function (index, element) {
-  console.log("foreach function works");
+  
   $("<option>")
     .text(element)
     .attr("value", element)
     .appendTo(fromLabel)
-   
+  //  console.log("value index is",index);
     
 });
 
 $.each(countryList, function (index, element) {
-  console.log("foreach function works");
+  // console.log("foreach function works");
   $("<option>")
     .text(element)
     .attr("value", element)
@@ -31,19 +35,40 @@ $.each(countryList, function (index, element) {
     
 });
 
+convert_Btn.on('click',function(){
+  console.log("button is clicked")
+  console.log("inputAmount is",inputAmount.val())
 
-// fetch('https://v6.exchangerate-api.com/v6/'+api+'/pair/'+base_currency+'/'+target_currency)
-// .then(res => res.json())
-// .then(data => {console.log(data);
-// rate.text(data.conversion_rate);
-// }
+  base_currency =fromLabel.val()
+ target_currency=toLabel.val()
 
-// )
-// .catch(err=>err())
+ console.log("from label is",base_currency)
+ console.log("To label is",target_currency)
 
-function convert(){
-    console.log("inputAmount is", inputAmount.val());
-    let target_result = inputAmount.val() * parseFloat(rate.text());
-    console.log("target result is", target_result);
-    result.text(target_result)
+fetch('https://v6.exchangerate-api.com/v6/'+api+'/pair/'+base_currency+'/'+target_currency)
+.then(res => res.json())
+.then(data => {console.log(data);
+rate.text(data.conversion_rate);
+console.log("type of inputAmount is",typeof(inputAmount.val()))
+let inputAmountNumber=parseFloat(inputAmount.val())
+let resultNumber=inputAmountNumber*rate
+console.log("result number is",typeof(resultNumber))
+let finalMoney=resultNumber.toString()
+// console.log("type of resultNumber is",finalMoney)
+console.log("resultNumber is",resultNumber)
+result.text(finalMoney)
 }
+
+)
+.catch(err=>err())  
+
+})
+
+
+
+
+
+
+
+
+
