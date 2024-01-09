127c26b16fa9f527253052beedea3817c8b72fb5
timo9939 <timothylau20199@gmail.com>
2024-01-09 17:23:01 +0000

    exchange rate and the output amount is correct

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ const fromLabel= $("#base_currency_container")
 const toLabel=$('#target_currency_container')
 let convert_Btn=$('#convertBtn')
 
-
+let resultNumber
 
 
   // let GBPinput=prompt("Enter GBP amount")
@@ -48,27 +48,34 @@ convert_Btn.on('click',function(){
 fetch('https://v6.exchangerate-api.com/v6/'+api+'/pair/'+base_currency+'/'+target_currency)
 .then(res => res.json())
 .then(data => {console.log(data);
+ 
 rate.text(data.conversion_rate);
 console.log("type of inputAmount is",typeof(inputAmount.val()))
 let inputAmountNumber=parseFloat(inputAmount.val())
-let resultNumber=inputAmountNumber*rate
-console.log("result number is",typeof(resultNumber))
-let finalMoney=resultNumber.toString()
-// console.log("type of resultNumber is",finalMoney)
+console.log("type of inputamountNumber is", typeof(inputAmountNumber),"and inputamountNumber is",inputAmountNumber)
+const rate_num_form=(data.conversion_rate)
+console.log("rate_num_form is",rate_num_form)
+resultNumber=(inputAmountNumber*rate_num_form)
 console.log("resultNumber is",resultNumber)
-result.text(finalMoney)
-}
+console.log("result number is",typeof(resultNumber))
 
-)
-.catch(err=>err())  
+result.text(resultNumber)
+})
 
+// .then( ()=>{
+//   let resultNumber=inputAmountNumber*rate
+//   console.log("resultNumber is",resultNumber)
+//   console.log("result number is",typeof(resultNumber))})
+
+.catch(err=>err())
 })
 
 
 
+// let finalMoney=resultNumber.toString()
+// console.log("type of resultNumber is",finalMoney)
 
-
-
+// result.text(finalMoney)
 
 
 
